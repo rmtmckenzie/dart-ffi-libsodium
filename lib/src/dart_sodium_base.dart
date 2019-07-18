f93c1eaa1e571b0fdfca5244b3d2808ab00bbb2e
Marvin Hannott <marvinhannott@gmail.com>
2019-07-18 23:27:40 +0200

    fixup! sodium init wrapper
    correct data type

--- lib/src/dart_sodium_base.dart
+++ lib/src/dart_sodium_base.dart
@@ -3,8 +3,8 @@ import 'dart:ffi' as ffi;
 final libsodium = ffi.DynamicLibrary.open("libsodium");
 var _isInitialized = false;
 
-final _init =
-    libsodium.lookupFunction<int Function(), int Function()>("sodium_init");
+final _init = libsodium
+    .lookupFunction<ffi.Int64 Function(), int Function()>("sodium_init");
 
 void init() {
   if (_isInitialized) {
