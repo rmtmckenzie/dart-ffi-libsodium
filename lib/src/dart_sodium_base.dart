8b0f90ee556298dbde08ba7fff578c6065557ad7
Marvin Hannott <marvinhannott@gmail.com>
2019-07-18 23:08:44 +0200

    sodium init wrapper

--- lib/src/dart_sodium_base.dart
+++ lib/src/dart_sodium_base.dart
@@ -5,3 +5,15 @@ var _isInitialized = false;
 
 final _init =
     libsodium.lookupFunction<int Function(), int Function()>("sodium_init");
+
+void init() {
+  if (_isInitialized) {
+    return;
+  }
+  final result = _init();
+  if (result < 0) {
+    throw Exception("""Initialization of sodium failed: $result 
+        Don't rely on dart_sodium at this point because its safety cannot be guaranteed""");
+  }
+  _isInitialized = true;
+}
