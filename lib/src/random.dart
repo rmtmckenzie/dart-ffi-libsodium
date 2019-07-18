3d8e6ee9e103d4a465cea406eb746792dbd75687
Marvin Hannott <marvinhannott@gmail.com>
2019-07-18 23:33:02 +0200

    fixup! implement randomBytesBuf
    use proper type

--- lib/src/random.dart
+++ lib/src/random.dart
@@ -3,16 +3,16 @@ import 'dart:typed_data';
 import 'dart:ffi';
 
 final _randomBytesBuf = libsodium.lookupFunction<
-    void Function(Pointer<void> buf, Uint64 size),
-    void Function(Pointer<void> buf, int size)>("randombytes_buf");
+    Void Function(Pointer<Uint8> buf, Uint64 size),
+    void Function(Pointer<Uint8> buf, int size)>("randombytes_buf");
 
 Uint8List randomBytesBuf(int size) {
-  final Pointer<void> bufptr = allocate(count: size);
+  final Pointer<Uint8> bufptr = allocate(count: size);
   try {
     _randomBytesBuf(bufptr, size);
     final buf = Uint8List(size);
     for (var i = 0; i < size; i++) {
-      buf[i] = bufptr.elementAt(i).cast();
+      buf[i] = bufptr.elementAt(i).load();
     }
     return buf;
   } finally {
