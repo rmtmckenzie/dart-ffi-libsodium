5019971bc1af3d9f73d80dcb391263e7d5a1a038
Marvin Hannott <marvinhannott@gmail.com>
2019-07-20 01:52:14 +0200

    export password_hashing

--- lib/dart_sodium.dart
+++ lib/dart_sodium.dart
@@ -5,5 +5,6 @@ library dart_sodium;
 
 export 'src/dart_sodium_base.dart';
 export 'src/random.dart';
+export 'src/password_hashing.dart';
 
 // TODO: Export any libraries intended for clients of this package.
