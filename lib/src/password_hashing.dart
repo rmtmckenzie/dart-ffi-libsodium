66cb7deb9aa144907378b1b30e868c40ca4f10da
Marvin Hannott <marvinhannott@gmail.com>
2019-07-31 21:05:01 +0200

    implement all opslimits and memlimits

--- lib/src/password_hashing.dart
+++ lib/src/password_hashing.dart
@@ -17,16 +17,48 @@ final _STRBYTES = libsodium.lookupFunction<Uint64 Function(), int Function()>(
 final _OPSLIMIT_MODERATE =
     libsodium.lookupFunction<Uint64 Function(), int Function()>(
         "crypto_pwhash_opslimit_moderate")();
+final _OPSLIMIT_MIN =
+    libsodium.lookupFunction<Uint64 Function(), int Function()>(
+        "crypto_pwhash_opslimit_min")();
+final _OPSLIMIT_SENSITIVE =
+    libsodium.lookupFunction<Uint64 Function(), int Function()>(
+        "crypto_pwhash_opslimit_sensitive")();
+final _OPSLIMIT_INTERACTIVE =
+    libsodium.lookupFunction<Uint64 Function(), int Function()>(
+        "crypto_pwhash_opslimit_interactive")();
+final _OPSLIMIT_MAX =
+    libsodium.lookupFunction<Uint64 Function(), int Function()>(
+        "crypto_pwhash_opslimit_max")();
 final _MEMLIMIT_MODERATE =
     libsodium.lookupFunction<Uint64 Function(), int Function()>(
         "crypto_pwhash_memlimit_moderate")();
+final _MEMLIMIT_MIN =
+    libsodium.lookupFunction<Uint64 Function(), int Function()>(
+        "crypto_pwhash_memlimit_min")();
+final _MEMLIMIT_SENSITIVE =
+    libsodium.lookupFunction<Uint64 Function(), int Function()>(
+        "crypto_pwhash_memlimit_sensitive")();
+final _MEMLIMIT_INTERACTIVE =
+    libsodium.lookupFunction<Uint64 Function(), int Function()>(
+        "crypto_pwhash_memlimit_interactive")();
+final _MEMLIMIT_MAX =
+    libsodium.lookupFunction<Uint64 Function(), int Function()>(
+        "crypto_pwhash_memlimit_max")();
 
-enum Opslimit { moderate }
-enum Memlimit { moderate }
+enum Opslimit { min, interactive, moderate, sensitive, max }
+enum Memlimit { min, interactive, moderate, sensitive, max }
 int getOpslimit(Opslimit opslimit) {
   switch (opslimit) {
     case Opslimit.moderate:
       return _OPSLIMIT_MODERATE;
+    case Opslimit.interactive:
+      return _OPSLIMIT_INTERACTIVE;
+    case Opslimit.min:
+      return _OPSLIMIT_MIN;
+    case Opslimit.sensitive:
+      return _OPSLIMIT_SENSITIVE;
+    case Opslimit.max:
+      return _OPSLIMIT_MAX;
     default:
       throw ArgumentError("Invalid value $opslimit");
   }
@@ -36,6 +68,14 @@ int getMemlimit(Memlimit memlimit) {
   switch (memlimit) {
     case Memlimit.moderate:
       return _MEMLIMIT_MODERATE;
+    case Memlimit.min:
+      return _MEMLIMIT_MIN;
+    case Memlimit.interactive:
+      return _MEMLIMIT_INTERACTIVE;
+    case Memlimit.sensitive:
+      return _MEMLIMIT_SENSITIVE;
+    case Memlimit.max:
+      return _MEMLIMIT_MAX;
     default:
       throw ArgumentError("Invalid value $memlimit");
   }
