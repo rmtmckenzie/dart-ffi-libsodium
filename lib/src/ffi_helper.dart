e76a07d64332464cefd6cccba0178b795e9c61e9
Marvin Hannott <marvinhannott@gmail.com>
2019-07-31 20:56:05 +0200

    change Uint8 pointer to Int8

--- lib/src/ffi_helper.dart
+++ lib/src/ffi_helper.dart
@@ -2,19 +2,20 @@ import 'dart:ffi';
 import 'dart:convert';
 import 'dart:typed_data';
 
-Pointer<Uint8> StringToCstr(String str) {
-  final buf = utf8.encode(str);
-  final Pointer<Uint8> ptr = allocate(count: str.length);
+Pointer<Int8> StringToCstr(String str) {
+  final buf = ascii.encode(str);
+  final Pointer<Int8> ptr = allocate(count: str.length);
   for (var i = 0; i < str.length; i++) {
     ptr.elementAt(i).store(buf[i]);
   }
+
   return ptr;
 }
 
-String CstrToString(Pointer<Uint8> ptr, int length) {
-  final buf = Uint8List(length);
+String CstrToString(Pointer<Int8> ptr, int length) {
+  final buf = Int8List(length);
   for (var i = 0; i < length; i++) {
     buf[i] = ptr.elementAt(i).load();
   }
-  return utf8.decode(buf);
+  return ascii.decode(buf);
 }
