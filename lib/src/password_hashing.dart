064c880379f1f8d1f686f325cdfd8757ef64bc38
Marvin Hannott <marvinhannott@gmail.com>
2019-07-31 20:57:08 +0200

    implement opslimit and memlimit constants

--- lib/src/password_hashing.dart
+++ lib/src/password_hashing.dart
@@ -4,9 +4,9 @@ import './ffi_helper.dart';
 import './dart_sodium_base.dart';
 import 'package:meta/meta.dart';
 
-typedef _PwhashStrNative = Int8 Function(Pointer<Uint8> out,
-    Pointer<Uint8> passwd, Uint64 passwdLen, Uint64 opsLimit, Uint64 memlimit);
-typedef _PwhashStrDart = int Function(Pointer<Uint8> out, Pointer<Uint8> passwd,
+typedef _PwhashStrNative = Int16 Function(Pointer<Int8> out,
+    Pointer<Int8> passwd, Uint64 passwdLen, Uint64 opsLimit, Uint64 memlimit);
+typedef _PwhashStrDart = int Function(Pointer<Int8> out, Pointer<Int8> passwd,
     int passwdLen, int opsLimit, int memlimit);
 
 final _pwhashStr = libsodium
@@ -14,18 +14,45 @@ final _pwhashStr = libsodium
 
 final _STRBYTES = libsodium.lookupFunction<Uint64 Function(), int Function()>(
     "crypto_pwhash_strbytes")();
+final _OPSLIMIT_MODERATE =
+    libsodium.lookupFunction<Uint64 Function(), int Function()>(
+        "crypto_pwhash_opslimit_moderate")();
+final _MEMLIMIT_MODERATE =
+    libsodium.lookupFunction<Uint64 Function(), int Function()>(
+        "crypto_pwhash_memlimit_moderate")();
 
-String pwhashStr(String passwd,
-    {@required int opslimit, @required int memlimit}) {
-  final Pointer<Uint8> out = allocate(count: _STRBYTES);
+enum Opslimit { moderate }
+enum Memlimit { moderate }
+int getOpslimit(Opslimit opslimit) {
+  switch (opslimit) {
+    case Opslimit.moderate:
+      return _OPSLIMIT_MODERATE;
+    default:
+      throw ArgumentError("Invalid value $opslimit");
+  }
+}
+
+int getMemlimit(Memlimit memlimit) {
+  switch (memlimit) {
+    case Memlimit.moderate:
+      return _MEMLIMIT_MODERATE;
+    default:
+      throw ArgumentError("Invalid value $memlimit");
+  }
+}
+
+String pwhashStr(String passwd, Opslimit opslimit, Memlimit memlimit) {
+  final realOpslimit = getOpslimit(opslimit);
+  final realMemlimit = getMemlimit(memlimit);
+  final out = allocate<Int8>(count: _STRBYTES);
   final passwdCstr = StringToCstr(passwd);
   try {
     final hashResult =
-        _pwhashStr(out, passwdCstr, passwd.length, opslimit, memlimit);
+        _pwhashStr(out, passwdCstr, passwd.length, realOpslimit, realMemlimit);
     if (hashResult < 0) {
-      Exception("dart_sodium pwhashStr failed: $hashResult");
+      throw Exception("dart_sodium pwhashStr failed: $hashResult");
     }
-    return CstrToString(out, passwd.length);
+    return CstrToString(out, _STRBYTES);
   } finally {
     out.free();
     passwdCstr.free();
