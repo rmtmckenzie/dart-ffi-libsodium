fda29d1fe539350b34e728e71116caba8b0839b0
Marvin Hannott <marvinhannott@gmail.com>
2019-08-22 23:44:29 +0200

    give init a description and explain why sodium must be initialized

--- lib/src/dart_sodium_base.dart
+++ lib/src/dart_sodium_base.dart
@@ -6,6 +6,12 @@ var _isInitialized = false;
 final _init = libsodium
     .lookupFunction<ffi.Int8 Function(), int Function()>("sodium_init");
 
+/// Initialize sodium. This function should be called before any other function of sodium.
+/// Calling this function several times has no effect and can be safely done.
+/// Initialization is required for the randomn number generator to work correctly
+/// and sodium to be thread safe. Failing to initialize sodium could result in unsafe results.
+/// Should sodium (for whatever reasons) fail to initialize, you should disregard any vaulues
+/// acquired from it.
 void init() {
   if (_isInitialized) {
     return;
