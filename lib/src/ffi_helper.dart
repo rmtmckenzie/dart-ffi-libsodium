abfc430e04415f8bf36378eef8369e170ab46fc7
Marvin Hannott <marvinhannott@gmail.com>
2019-08-22 23:58:17 +0200

    description for UnsignedCharToBuffer

--- lib/src/ffi_helper.dart
+++ lib/src/ffi_helper.dart
@@ -40,6 +40,10 @@ Pointer<Uint8> BufferToUnsignedChar(Uint8List buf) {
   return ptr;
 }
 
+/// Returns a buffer with the content of heap memory.
+/// The buffer is [length] long and reading from heap begins at [ptr].
+/// Reading from heap is not null terminated. If [length] is too long information
+/// could be leaked into the application.
 Uint8List UnsignedCharToBuffer(Pointer<Uint8> ptr, int length) {
   final buf = Uint8List(length);
   for (var i = 0; i < length; i++) {
