0ce4650326e77e95a6130a616f7bcd33b86d0439
Marvin Hannott <marvinhannott@gmail.com>
2019-08-22 23:30:50 +0200

    clarify the length of the nonce and the function for decryption

--- lib/src/authenticated_encryption.dart
+++ lib/src/authenticated_encryption.dart
@@ -64,9 +64,11 @@ Uint8List secretBoxKeygen() {
 
 /// Use for encrypting any kind of data with the XSalsa20 stream cipher.
 /// An authentication tag (Poly1305 MAC) is added to the ciphertext and hasn't to be stored seperately.
-/// The [nonce] can be generated with any cryptographic randomn number generator like [randomnBytesBuf]
+/// The [nonce] must be [secretBoxNonceBytes] long and can be generated
+/// with any cryptographic randomn number generator like [randomnBytesBuf]
 /// or be obtained by another secure scheme like an atomic counter, but it must never be the same.
-/// The [nonce] needs to be stored alongside the ciphertext for decryption and doesn't need to be secret.
+/// The [nonce] needs to be stored alongside the ciphertext for decryption with [secretBoxOpenEasy]
+/// and doesn't need to be secret.
 /// The [key] has to be [secretBoxKeyBytes] long.
 ///
 /// ```
