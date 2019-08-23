73de7af517c67f2926e91c86309c8966e65d7dbb
Marvin Hannott <marvinhannott@gmail.com>
2019-08-23 02:50:25 +0200

    export authentication library

--- lib/dart_sodium.dart
+++ lib/dart_sodium.dart
@@ -7,5 +7,6 @@ export 'src/dart_sodium_base.dart' show init;
 export 'src/random.dart';
 export 'src/password_hashing.dart';
 export 'src/authenticated_encryption.dart';
+export 'src/authentication.dart';
 
 // TODO: Export any libraries intended for clients of this package.
