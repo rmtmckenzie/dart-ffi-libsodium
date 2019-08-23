a80deba1f75561969f59ced641f16170377d4fae
Marvin Hannott <marvinhannott@gmail.com>
2019-08-23 02:50:57 +0200

    remove comments generated by stagehand

--- lib/dart_sodium.dart
+++ lib/dart_sodium.dart
@@ -1,6 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library dart_sodium;
 
 export 'src/dart_sodium_base.dart' show init;
@@ -8,5 +5,3 @@ export 'src/random.dart';
 export 'src/password_hashing.dart';
 export 'src/authenticated_encryption.dart';
 export 'src/authentication.dart';
-
-// TODO: Export any libraries intended for clients of this package.
