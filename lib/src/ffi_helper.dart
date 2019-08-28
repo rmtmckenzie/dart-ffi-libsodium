a298139cfee59cf1dd23733778003f30883b1a93
Marvin Hannott <marvinhannott@gmail.com>
2019-08-28 08:45:59 +0200

    remove depricated functions

--- lib/src/ffi_helper.dart
+++ lib/src/ffi_helper.dart
@@ -1,28 +1,5 @@
 import 'dart:ffi';
-import 'dart:convert';
 import 'dart:typed_data';
-
-Pointer<Int8> StringToCstr(String str) {
-  final buf = ascii.encode(str);
-  final Pointer<Int8> ptr = allocate(count: str.length + 1);
-  for (var i = 0; i < str.length; i++) {
-    ptr.elementAt(i).store(buf[i]);
-  }
-  ptr.elementAt(str.length).store(0);
-  return ptr;
-}
-
-String CstrToString(Pointer<Int8> ptr, int length) {
-  final buf = Int8List(length);
-  for (var i = 0; i < length; i++) {
-    int char = ptr.elementAt(i).load();
-    if (char == 0) {
-      break;
-    }
-    buf[i] = char;
-  }
-  return ascii.decode(buf);
-}
 
 /// Allocates heap memory with the length of [buf] and fills it with its content.
 /// A pointer to the first char gets returned.
