5ee2dc8c82a647660fc797d2fd0ef10f5ddf7c30
Marvin Hannott <marvinhannott@gmail.com>
2019-08-28 10:11:00 +0200

    make auth bindings public

--- lib/src/bindings/auth.dart
+++ lib/src/bindings/auth.dart
@@ -5,12 +5,11 @@ import 'package:dart_sodium/src/ffi_helper.dart';
 import '../dart_sodium_base.dart';
 import 'dart:ffi';
 
-final _authKeyGen =
+final authKeyGen =
     libsodium.lookupFunction<Void Function(CString), void Function(CString)>(
         "crypto_auth_keygen");
 
-/// Length of the authentication tag.
-final _authBytes = libsodium
+final authBytes = libsodium
     .lookupFunction<Uint64 Function(), int Function()>("crypto_auth_bytes")();
 
 typedef _AuthNative = Void Function(
@@ -18,16 +17,11 @@ typedef _AuthNative = Void Function(
 typedef _AuthDart = void Function(
     Pointer<Uint8> out, Pointer<Uint8> msg, int msglen, Pointer<Uint8> key);
 
-final _auth = libsodium.lookupFunction<_AuthNative, _AuthDart>("crypto_auth");
+final auth = libsodium.lookupFunction<_AuthNative, _AuthDart>("crypto_auth");
 
 typedef _AuthVerifyNative = Int32 Function(
     Pointer<Uint8> tag, Pointer<Uint8> msg, Uint64 msglen, Pointer<Uint8> key);
 typedef _AuthVerifyDart = int Function(
     Pointer<Uint8> tag, Pointer<Uint8> msg, int msglen, Pointer<Uint8> key);
-final _authVerify = libsodium
+final authVerify = libsodium
     .lookupFunction<_AuthVerifyNative, _AuthVerifyDart>("crypto_auth_verify");
-
-main(List<String> args) {
-  Pointer<CString> ptr = allocate();
-  print(ptr);
-}
