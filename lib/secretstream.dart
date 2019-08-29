815b022a8397ce6a7b8e079ed6d383b1079e49dc
Marvin Hannott <marvinhannott@gmail.com>
2019-08-29 22:22:11 +0200

    split secretstream encrypter and decrypter

--- lib/secretstream.dart
+++ lib/secretstream.dart
@@ -1,125 +1,133 @@
 import 'dart:convert';
 import 'dart:ffi';
 import 'dart:typed_data';
+import 'package:dart_sodium/src/ffi_helper.dart';
 
-class _SecretBoxData {
-  final Pointer<Uint8> _header;
-  final Pointer<_State> _state;
-  const _SecretBoxData(this._state, this._header);
-  Uint8List get header => UnsignedCharToBuffer(_header, _headerBytes);
-}
+import 'dart_sodium.dart';
+import 'src/bindings/secretstream.dart' as bindings;
 
-Uint8List keyGen() {
-  Pointer<Uint8> keyPtr;
-  try {
-    keyPtr = allocate(count: keyBytes);
-    _keyGen(keyPtr);
-    return UnsignedCharToBuffer(keyPtr, keyBytes);
-  } finally {
-    keyPtr?.free();
+class Encrypter {
+  static Uint8List keyGen() {
+    Pointer<Uint8> keyPtr;
+    try {
+      keyPtr = allocate(count: bindings.keyBytes);
+      bindings.keyGen(keyPtr);
+      return CStringToBuffer(keyPtr, bindings.keyBytes);
+    } finally {
+      keyPtr.free();
+    }
   }
-}
 
-_SecretBoxData initPush(Uint8List key) {
-  assert(key.length == keyBytes, "Key hasn't expected length [keyBytes]");
-  Pointer<Uint8> keyPtr, headerPtr;
-  Pointer<_State> statePtr;
-  try {
-    keyPtr = BufferToUnsignedChar(key);
-    headerPtr = allocate(count: _headerBytes);
-    statePtr = allocate(count: _stateBytes);
-    int initResult = _initPush(statePtr, headerPtr, keyPtr);
+  final Pointer<Uint8> _key;
+  Pointer<Uint8> _header;
+  Pointer<bindings.State> _state;
+  Uint8List get header => CStringToBuffer(_header, bindings.headerBytes);
+
+  Encrypter(Uint8List key) : _key = BufferToCString(key) {
+    if (key.length != bindings.keyBytes) {
+      _key.free();
+      throw Exception("Key hasn't expected length");
+    }
+    _header = allocate(count: bindings.headerBytes);
+    _state = allocate(count: bindings.stateBytes);
+    int initResult = bindings.initPush(_state, _header, _key);
     if (initResult != 0) {
-      headerPtr?.free();
+      close();
       throw Exception("SecretBox init failed");
     }
-    return _SecretBoxData(statePtr, headerPtr);
-  } finally {
-    keyPtr?.free();
+  }
+
+  Uint8List push(Uint8List data,
+      {Uint8List additionalData, Tag tag = Tag.message}) {
+    Pointer<Uint8> dataPtr, adPtr, cPtr;
+    try {
+      dataPtr = BufferToCString(data);
+      final cLen = data.length + bindings.aBytes;
+      cPtr = allocate(count: cLen);
+      var adLen = 0;
+      if (additionalData != null) {
+        adLen = additionalData.length;
+        adPtr = BufferToCString(additionalData);
+      }
+      int pushResult = bindings.push(
+          _state, cPtr, null, dataPtr, data.length, adPtr, adLen, tag.index);
+      if (pushResult != 0) {
+        throw Exception("SecretBox push failed");
+      }
+      return CStringToBuffer(cPtr, cLen);
+    } finally {
+      dataPtr?.free();
+      adPtr?.free();
+      cPtr?.free();
+    }
+  }
+
+  void close() {
+    _key.free();
+    _header.free();
+    _state.free();
   }
 }
 
-Uint8List push(Uint8List data, _SecretBoxData state,
-    [Uint8List additionalData, int tag = 0]) {
-  Pointer<Uint8> dataPtr, adPtr, cPtr;
-  try {
-    dataPtr = BufferToUnsignedChar(data);
-    final cLen = data.length + _aBytes;
-    cPtr = allocate(count: cLen);
-    var adLen = 0;
-    if (additionalData != null) {
-      adLen = additionalData.length;
-      adPtr = BufferToUnsignedChar(additionalData);
-    }
+enum Tag { message, push, rekey, finish }
 
-    int pushResult = _secretStreamPush(
-        state._state, cPtr, null, dataPtr, data.length, adPtr, adLen, tag);
-    if (pushResult != 0) {
-      throw Exception("SecretBox push failed");
-    }
-    return UnsignedCharToBuffer(cPtr, cLen);
-  } finally {
-    dataPtr?.free();
-    adPtr?.free();
-    cPtr?.free();
-  }
+class _PullData {
+  final Uint8List decryptedChunk, additionalData;
+  final Tag tag;
+  const _PullData(this.decryptedChunk, this.additionalData, this.tag);
 }
 
-_SecretBoxData initPull(Uint8List key, Uint8List header) {
-  assert(key.length == keyBytes, "Key hasn't expected length");
-  assert(header.length == _headerBytes, "Header hasn't expected length");
-  Pointer<Uint8> keyPtr, headerPtr;
-  Pointer<_State> statePtr;
-  try {
-    keyPtr = BufferToUnsignedChar(key);
-    headerPtr = BufferToUnsignedChar(header);
-    statePtr = allocate(count: _stateBytes);
-    int initResult = _initPull(statePtr, headerPtr, keyPtr);
+class Decrypter {
+  final Pointer<Uint8> _key;
+  Pointer<Uint8> _header;
+  Pointer<bindings.State> _state;
+
+  Decrypter(Uint8List key, Uint8List header)
+      : _key = BufferToCString(key),
+        _header = BufferToCString(header) {
+    if (key.length != bindings.keyBytes) {
+      _key.free();
+      throw Exception("Key hasn't expected length");
+    }
+    assert(
+        header.length == bindings.headerBytes, "Header hasn't expected length");
+    _state = allocate(count: bindings.stateBytes);
+    int initResult = bindings.initPull(_state, _header, _key);
     if (initResult != 0) {
-      headerPtr?.free();
+      close();
       throw Exception("SecretBox init failed");
     }
-    return _SecretBoxData(statePtr, null);
-  } finally {
-    keyPtr?.free();
-    headerPtr?.free();
   }
-}
 
-Uint8List pull(_SecretBoxData state, Uint8List ciphertext) {
-  Pointer<Uint8> dataPtr, adPtr, cPtr, tagPtr;
-  try {
-    final dataLen = ciphertext.length - _aBytes;
-    dataPtr = allocate(count: dataLen);
-    cPtr = BufferToUnsignedChar(ciphertext);
-    tagPtr = allocate();
-    adPtr = allocate();
+  _PullData pull(Uint8List ciphertext) {
+    Pointer<Uint8> dataPtr, adPtr, cPtr, tagPtr;
+    try {
+      final dataLen = ciphertext.length - bindings.aBytes;
+      dataPtr = allocate(count: dataLen);
+      cPtr = BufferToCString(ciphertext);
+      tagPtr = allocate();
+      adPtr = allocate();
 
-    int pushResult = _secretStreamPull(
-        state._state, dataPtr, null, tagPtr, cPtr, ciphertext.length, adPtr, 0);
-    if (pushResult != 0) {
-      throw Exception("SecretBox pull failed");
+      int pushResult = bindings.pull(
+          _state, dataPtr, null, tagPtr, cPtr, ciphertext.length, adPtr, 0);
+      if (pushResult != 0) {
+        throw Exception("SecretBox pull failed");
+      }
+      final chunk = CStringToBuffer(dataPtr, dataLen);
+      final adData = CStringToBuffer(adPtr, 0);
+      final tag = tagPtr.load<int>();
+      return _PullData(chunk, adData, Tag.values[tag]);
+    } finally {
+      dataPtr.free();
+      adPtr.free();
+      tagPtr.free();
+      cPtr.free();
     }
-    print(tagPtr.load<int>());
-    return UnsignedCharToBuffer(dataPtr, dataLen);
-  } finally {
-    dataPtr?.free();
-    adPtr?.free();
-    tagPtr?.free();
-    cPtr?.free();
   }
-}
 
-void main(List<String> args) {
-  init();
-  final key = keyGen();
-  print(key);
-  final state = initPush(key);
-  print(state);
-  final c1 = push(ascii.encode("hello"), state, null, _tagFinal);
-  print(c1);
-
-  final pstate = initPull(key, state.header);
-  final p1 = pull(pstate, c1);
-  print(p1);
+  void close() {
+    _key.free();
+    _header.free();
+    _state.free();
+  }
 }
