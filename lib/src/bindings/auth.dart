f0b3af6bae2e040f376539333fd54c36f8597ff2
Marvin Hannott <marvinhannott@gmail.com>
2019-08-31 08:07:03 +0200

    remove unnecessary import

--- lib/src/bindings/auth.dart
+++ lib/src/bindings/auth.dart
@@ -1,5 +1,3 @@
-import 'package:dart_sodium/src/ffi_helper.dart';
-
 import '../dart_sodium_base.dart';
 import 'dart:ffi';
 
@@ -8,8 +6,8 @@ final keyGen = libsodium.lookupFunction<Void Function(Pointer<Uint8>),
 
 final authBytes = libsodium
     .lookupFunction<Uint64 Function(), int Function()>("crypto_auth_bytes")();
-final keyBytes = libsodium
-    .lookupFunction<Uint64 Function(), int Function()>("crypto_auth_key")();
+final keyBytes = libsodium.lookupFunction<Uint64 Function(), int Function()>(
+    "crypto_auth_keybytes")();
 
 typedef _AuthNative = Int16 Function(
     Pointer<Uint8> out, Pointer<Uint8> msg, Uint64 msglen, Pointer<Uint8> key);
