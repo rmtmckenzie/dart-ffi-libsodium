63b27954b1372a413968da57f8037e5f46a691e1
Marvin Hannott <marvinhannott@gmail.com>
2019-08-31 11:23:57 +0200

    set nonce length to be nonceBytes

--- test/secretbox_test.dart
+++ test/secretbox_test.dart
@@ -1,21 +1,23 @@
 import 'dart:convert';
 
+import 'package:dart_sodium/dart_sodium.dart';
 import 'package:dart_sodium/secretbox.dart';
 import 'package:dart_sodium/random.dart' as rand;
+import 'package:dart_sodium/helpers.dart';
 import 'package:test/test.dart';
 
 main() {
   test("encrypt and decrypt easy", () {
+    init();
     SecretBox box;
     try {
       final key = SecretBox.keyGen();
       box = SecretBox(key);
       final msg = utf8.encode("my message");
-      final nonce = rand.buffer(16);
+      final nonce = rand.buffer(SecretBox.nonceBytes);
       final ciphertext = box.easy(msg, nonce);
-
       final cleartext = box.openEasy(ciphertext, nonce);
-      expect(msg, cleartext);
+      expect(memoryCompare(msg, cleartext), true);
     } finally {
       box.close();
     }
