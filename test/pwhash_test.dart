f716da4416fc83674127af530fd975c38dfd22c6
Marvin Hannott <marvinhannott@gmail.com>
2019-08-31 08:32:16 +0200

    pwhash test

---
+++ test/pwhash_test.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+
+import 'package:dart_sodium/dart_sodium.dart';
+import 'package:dart_sodium/pwhash.dart';
+import 'package:test/test.dart';
+
+main() {
+  test('store and verify', () {
+    init();
+    final pwd = utf8.encode("my password");
+    final hash = store(pwd, OpsLimit.min, MemLimit.min);
+    final isValid = storeVerify(hash, pwd);
+    expect(isValid, true);
+  });
+}
