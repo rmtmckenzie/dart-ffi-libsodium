8cb91cf0fa9d8a9fff158f18ff2961358220a855
Marvin Hannott <marvinhannott@gmail.com>
2019-09-03 18:47:46 +0200

    don't store pointer to public key as field of box
    instead receive public key as function argument

--- lib/box.dart
+++ lib/box.dart
@@ -33,26 +33,22 @@ class Box {
     }
   }
 
-  final Pointer<Uint8> secretKey, publicKey;
-  Box(Uint8List publicKey, Uint8List secretKey)
-      : this.secretKey = BufferToCString(secretKey),
-        this.publicKey = BufferToCString(publicKey) {
+  final Pointer<Uint8> secretKey;
+  Box(Uint8List secretKey) : this.secretKey = BufferToCString(secretKey) {
     if (secretKey.length != bindings.secretKeyBytes) {
       throw Exception("Secret Key hasn't expected length");
     }
-    if (publicKey.length != bindings.publicKeyBytes) {
-      throw Exception("Public Key hasn't expected length");
-    }
   }
 
-  Uint8List easy(Uint8List msg, Uint8List nonce) {
+  Uint8List easy(Uint8List msg, Uint8List nonce, Uint8List publicKey) {
+    final pkPtr = BufferToCString(publicKey);
     final Pointer<Uint8> msgPtr = BufferToCString(msg);
     final Pointer<Uint8> noncePtr = BufferToCString(nonce);
     final cLen = bindings.macBytes + msg.length;
     final Pointer<Uint8> cPtr = allocate(count: cLen);
     try {
-      final result = bindings.easy(
-          cPtr, msgPtr, msg.length, noncePtr, publicKey, secretKey);
+      final result =
+          bindings.easy(cPtr, msgPtr, msg.length, noncePtr, pkPtr, secretKey);
       if (result != 0) {
         throw Exception("Encrypting failed");
       }
@@ -61,17 +57,20 @@ class Box {
       msgPtr.free();
       noncePtr.free();
       cPtr.free();
+      pkPtr.free();
     }
   }
 
-  Uint8List openEasy(Uint8List ciphertext, Uint8List nonce) {
+  Uint8List openEasy(
+      Uint8List ciphertext, Uint8List nonce, Uint8List publicKey) {
+    final pkPtr = BufferToCString(publicKey);
     final msgLen = ciphertext.length - bindings.macBytes;
     final Pointer<Uint8> msgPtr = allocate(count: msgLen);
     final Pointer<Uint8> noncePtr = BufferToCString(nonce);
     final Pointer<Uint8> cPtr = BufferToCString(ciphertext);
     try {
       final result = bindings.openEasy(
-          msgPtr, cPtr, ciphertext.length, noncePtr, publicKey, secretKey);
+          msgPtr, cPtr, ciphertext.length, noncePtr, pkPtr, secretKey);
       if (result != 0) {
         throw Exception("Decrypting failed");
       }
@@ -80,11 +79,11 @@ class Box {
       msgPtr.free();
       noncePtr.free();
       cPtr.free();
+      pkPtr.free();
     }
   }
 
   void close() {
-    publicKey.free();
     secretKey.free();
   }
 }
