25b19936a6a5f3f28fb86455a86262ba32bf56f1
Marvin Hannott <marvinhannott@gmail.com>
2019-09-03 21:58:03 +0200

    add class for box beforenm /afternm interface

--- lib/box.dart
+++ lib/box.dart
@@ -87,3 +87,70 @@ class Box {
     secretKey.free();
   }
 }
+
+class BoxNm {
+  final Pointer<Uint8> key;
+  BoxNm(Uint8List publicKey, Uint8List secretKey)
+      : key = allocate(count: bindings.beforeNmBytes) {
+    if (secretKey.length != bindings.secretKeyBytes) {
+      throw Exception("Secret Key hasn't expected length");
+    }
+    if (publicKey.length != bindings.publicKeyBytes) {
+      throw Exception("Public Key hasn't expected length");
+    }
+    final skPtr = BufferToCString(secretKey);
+    final pkPtr = BufferToCString(publicKey);
+    try {
+      final result = bindings.beforeNm(key, pkPtr, skPtr);
+      if (result != 0) {
+        key.free();
+        throw Exception("Key generation failed");
+      }
+    } finally {
+      skPtr.free();
+      pkPtr.free();
+    }
+  }
+
+  Uint8List easy(Uint8List msg, Uint8List nonce) {
+    final Pointer<Uint8> msgPtr = BufferToCString(msg);
+    final Pointer<Uint8> noncePtr = BufferToCString(nonce);
+    final cLen = bindings.macBytes + msg.length;
+    final Pointer<Uint8> cPtr = allocate(count: cLen);
+    try {
+      final result =
+          bindings.easyAfterNm(cPtr, msgPtr, msg.length, noncePtr, key);
+      if (result != 0) {
+        throw Exception(r"Encrypting failed");
+      }
+      return CStringToBuffer(cPtr, cLen);
+    } finally {
+      msgPtr.free();
+      noncePtr.free();
+      cPtr.free();
+    }
+  }
+
+  Uint8List openEasy(Uint8List ciphertext, Uint8List nonce) {
+    final msgLen = ciphertext.length - bindings.macBytes;
+    final Pointer<Uint8> msgPtr = allocate(count: msgLen);
+    final Pointer<Uint8> noncePtr = BufferToCString(nonce);
+    final Pointer<Uint8> cPtr = BufferToCString(ciphertext);
+    try {
+      final result = bindings.openEasyAfterNm(
+          msgPtr, cPtr, ciphertext.length, noncePtr, key);
+      if (result != 0) {
+        throw Exception("Decrypting failed");
+      }
+      return CStringToBuffer(msgPtr, msgLen);
+    } finally {
+      msgPtr.free();
+      noncePtr.free();
+      cPtr.free();
+    }
+  }
+
+  void close() {
+    key.free();
+  }
+}
