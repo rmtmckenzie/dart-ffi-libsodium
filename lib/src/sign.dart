a193c681b248ee1ecfade7f4748c11fac85a4abf
Marvin Hannott <marvinhannott@gmail.com>
2019-09-04 17:42:41 +0200

    make Signer.open a static method
    secretKey isn't required for that operation

--- lib/src/sign.dart
+++ lib/src/sign.dart
@@ -30,6 +30,27 @@ class Signer {
     }
   }
 
+  /// Checks the authenticity of [signedMsg] (signed with [sign]) with the [publicKey]
+  /// and returns only the message
+  static Uint8List open(Uint8List signedMsg, Uint8List publicKey) {
+    final msgLen = signedMsg.length - bindings.signBytes;
+    final Pointer<Uint8> msgPtr = allocate(count: msgLen);
+    final Pointer<Uint8> signedMsgPtr = BufferToCString(signedMsg);
+    final Pointer<Uint8> pkPtr = BufferToCString(publicKey);
+    try {
+      final result = bindings.signOpen(
+          msgPtr, null, signedMsgPtr, signedMsg.length, pkPtr);
+      if (result != 0) {
+        throw Exception(
+            "Open message failed. Signature doesn't seem to be valid");
+      }
+      return CStringToBuffer(msgPtr, msgLen);
+    } finally {
+      msgPtr.free();
+      signedMsgPtr.free();
+    }
+  }
+
   final Pointer<Uint8> _secretKey;
   Signer(Uint8List secretKey) : this._secretKey = BufferToCString(secretKey) {
     if (secretKey.length != bindings.secretKeyBytes) {
@@ -50,27 +71,6 @@ class Signer {
         throw Exception("Signing message failed");
       }
       return CStringToBuffer(signedMsgPtr, signedMsgLen);
-    } finally {
-      msgPtr.free();
-      signedMsgPtr.free();
-    }
-  }
-
-  /// Checks the authenticity of [signedMsg] (signed with [sign]) with the [publicKey]
-  /// and returns only the message
-  Uint8List open(Uint8List signedMsg, Uint8List publicKey) {
-    final msgLen = signedMsg.length - bindings.signBytes;
-    final Pointer<Uint8> msgPtr = allocate(count: msgLen);
-    final Pointer<Uint8> signedMsgPtr = BufferToCString(signedMsg);
-    final Pointer<Uint8> pkPtr = BufferToCString(publicKey);
-    try {
-      final result = bindings.signOpen(
-          msgPtr, null, signedMsgPtr, signedMsg.length, pkPtr);
-      if (result != 0) {
-        throw Exception(
-            "Open message failed. Signature doesn't seem to be valid");
-      }
-      return CStringToBuffer(msgPtr, msgLen);
     } finally {
       msgPtr.free();
       signedMsgPtr.free();
