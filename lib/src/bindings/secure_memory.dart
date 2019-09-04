850d280ab590d5e1fc5bd96cf4b5802c03bf8b59
Marvin Hannott <marvinhannott@gmail.com>
2019-09-04 21:58:07 +0200

    change Pointer<Void> to IntPtr for addr

--- lib/src/bindings/secure_memory.dart
+++ lib/src/bindings/secure_memory.dart
@@ -3,8 +3,8 @@ import 'dart:ffi';
 import '../dart_sodium_base.dart';
 
 final memoryLock = libsodium.lookupFunction<
-    Int16 Function(Pointer<Void> addr, IntPtr len),
-    int Function(Pointer<Void> addr, int len)>("sodium_mlock");
+    Int16 Function(IntPtr addr, IntPtr len),
+    int Function(int addr, int len)>("sodium_mlock");
 final memoryUnlock = libsodium.lookupFunction<
-    Int16 Function(Pointer<Void> addr, IntPtr len),
-    int Function(Pointer<Void> addr, int len)>("sodium_munlock");
+    Int16 Function(IntPtr addr, IntPtr len),
+    int Function(int addr, int len)>("sodium_munlock");
