dfde01a9b171d661b489658680f2d60ede97af1d
Marvin Hannott <marvinhannott@gmail.com>
2020-01-08 00:49:30 +0100

    huge performance increase: Use Int32xList instead of Uint8List

--- benchmark/dart_rng.dart
+++ benchmark/dart_rng.dart
@@ -5,13 +5,13 @@ import 'dart:math';
 
 class RandomBytesBenchmark extends BenchmarkBase {
   RandomBytesBenchmark() : super('RandomBytesBenchmark');
-
+  final rng = Random.secure();
+  final randomNumbers = Uint8List(32).buffer.asInt32x4List();
   @override
   void run() {
-    final rng = Random.secure();
-    final randomNumbers = Uint8List(32);
-    for (var i = 0; i < 32; i++) {
-      randomNumbers[i] = rng.nextInt(255);
+    for (var i = 0; i < randomNumbers.length; i++) {
+      randomNumbers[i] = Int32x4(rng.nextInt(255), rng.nextInt(255),
+          rng.nextInt(255), rng.nextInt(255));
     }
   }
 }
