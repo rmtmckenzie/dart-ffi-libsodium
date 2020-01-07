9197e92e4ac337ef8646b32b1c60929ac687ac51
Marvin Hannott <marvinhannott@gmail.com>
2020-01-07 17:29:25 +0100

    test for secret_box

---
+++ test/secret_box_test.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+
+import 'package:dart_sodium/secret_box.dart' as secret_box;
+import 'package:dart_sodium/random_bytes.dart' as rand_bytes;
+import 'package:dart_sodium/sodium.dart' as sodium;
+import 'package:test/test.dart';
+
+void main() {
+  test('encrypt and decrypt message as stream', () {
+    sodium.init();
+    final key = secret_box.keyGen();
+    final message = utf8.encode('hello world');
+    final nonce = rand_bytes.buffer(secret_box.nonceBytes);
+
+    final encrypted = secret_box.easy(message, nonce, key);
+    final decrypted = secret_box.openEasy(encrypted, nonce, key);
+
+    expect(message, decrypted);
+  });
+}
