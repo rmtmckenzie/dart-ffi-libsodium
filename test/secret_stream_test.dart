3bdfc33760c5cc3ce9ed5eb2848e0549c75c6d9b
Marvin Hannott <marvinhannott@gmail.com>
2020-01-07 17:37:09 +0100

    expand test to to consecutive messages

--- test/secret_stream_test.dart
+++ test/secret_stream_test.dart
@@ -9,6 +9,19 @@ void main() {
     sodium.init();
     final key = secret_stream.keyGen();
     final message = utf8.encode('hello world');
+    final message2 = utf8.encode('hello to the world');
+
+    final initPush = secret_stream.initPush(key);
+    final encChunk = secret_stream.push(initPush.state, message);
+    final encChunk2 = secret_stream.push(initPush.state, message2);
+
+    final pullState = secret_stream.initPull(initPush.header, key);
+    final decChunk = secret_stream.pull(pullState, encChunk);
+    final decChunk2 = secret_stream.pull(pullState, encChunk2);
+
+    expect(message, decChunk.msg);
+    expect(message2, decChunk2.msg);
+  });
 
     final initPush = secret_stream.initPush(key);
     final encChunk = secret_stream.push(initPush.state, message);
