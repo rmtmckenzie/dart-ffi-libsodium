746b2f5b36267efb182bf15db0b319e199d13724
Marvin Hannott <marvinhannott@gmail.com>
2020-01-08 20:55:32 +0100

    add test for generic hash stream

--- test/generic_hash_test.dart
+++ test/generic_hash_test.dart
@@ -19,4 +19,15 @@ void main() {
     final hashed = hash.genericHash(message);
     expect(hashed.length, hash.genericHashBytes);
   });
+
+  test('Hash multi part message with key', () {
+    final key = hash.keyGen();
+    final message = utf8.encode('hello world');
+    final message2 = utf8.encode('hello to the world');
+    final state = hash.init(key: key);
+    hash.update(state, message);
+    hash.update(state, message2);
+    final hashed = hash.finish(state);
+    expect(hashed.length, hash.genericHashBytes);
+  });
 }
