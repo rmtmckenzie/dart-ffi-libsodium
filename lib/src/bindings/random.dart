845a16deef62b0536d123abfd7e5ede9530d10be
Marvin Hannott <marvinhannott@gmail.com>
2020-01-08 01:28:42 +0100

    add bindings for random_bytes buffer_deterministic and seedbytes

--- lib/src/bindings/random.dart
+++ lib/src/bindings/random.dart
@@ -1,6 +1,14 @@
 import 'sodium.dart';
 import 'dart:ffi';
 
+final seedBytes = sodium
+    .lookupFunction<Uint64 Function(), int Function()>('randombytes_seedbytes');
+
 final buffer = sodium.lookupFunction<
     Void Function(Pointer<Void> buf, IntPtr size),
     void Function(Pointer<Void> buf, int size)>("randombytes_buf");
+
+final bufferDeterministic = sodium.lookupFunction<
+    Void Function(Pointer<Void> buf, IntPtr size, Pointer<Uint8>),
+    void Function(Pointer<Void> buf, int size,
+        Pointer<Uint8>)>('randombytes_buf_deterministic');
