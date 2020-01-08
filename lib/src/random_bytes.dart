4fe19a3160be1e37a2e015e16057a422d17d7e0b
Marvin Hannott <marvinhannott@gmail.com>
2020-01-08 01:29:11 +0100

    implement bufferDeterministic()

--- lib/src/random_bytes.dart
+++ lib/src/random_bytes.dart
@@ -12,3 +12,16 @@ UnmodifiableUint8ListView buffer(int size) {
   bufPtr.free();
   return UnmodifiableUint8ListView(Uint8List.fromList(bufPtr.view));
 }
+
+/// Generates a sequence of [size] pseudo-random numbers.
+/// [seed] must be [seedBytes] long. The same [seed] produces the same
+/// sequence of pseudo-random numbers.
+UnmodifiableUint8ListView bufferDeterministic(int size, Uint8List seed) {
+  final bufPtr = Uint8Array.allocate(count: size);
+  final seedPtr = Uint8Array.fromTypedList(seed);
+
+  bindings.bufferDeterministic(bufPtr.rawPtr.cast(), size, seedPtr.rawPtr);
+  bufPtr.free();
+  seedPtr.free();
+  return UnmodifiableUint8ListView(Uint8List.fromList(bufPtr.view));
+}
