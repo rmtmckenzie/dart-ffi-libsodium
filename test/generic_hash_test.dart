c2c9d8e5665a0833555788d063ffe829364e5501
Marvin Hannott <marvinhannott@gmail.com>
2020-01-08 05:21:50 +0100

    add test generic_hash

---
+++ test/generic_hash_test.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:dart_sodium/generic_hash.dart' as hash;
+import 'package:dart_sodium/sodium.dart' as sodium;
+import 'package:test/test.dart';
+
+void main() {
+  sodium.init();
+  test('Hash a message with key', () {
+    final key = hash.keyGen();
+    expect(key.length, hash.genericHashBytes);
+    final message = utf8.encode('hello world');
+    final hashed = hash.genericHash(message, key);
+    expect(hashed.length, hash.genericHashBytes);
+  });
+
+  test('Hash a message without key', () {
+    final message = utf8.encode('hello world');
+    final hashed = hash.genericHash(message);
+    expect(hashed.length, hash.genericHashBytes);
+  });
+}
