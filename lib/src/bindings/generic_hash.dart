39b5f519e4eb74ef6ef4f495a11fb9f423ec171a
Marvin Hannott <marvinhannott@gmail.com>
2020-01-08 02:41:35 +0100

    call functions for lookup constants immediately

--- lib/src/bindings/generic_hash.dart
+++ lib/src/bindings/generic_hash.dart
@@ -3,24 +3,24 @@ import 'dart:ffi';
 
 final genericHashBytes =
     sodium.lookupFunction<Uint64 Function(), int Function()>(
-        'crypto_generichash_bytes');
+        'crypto_generichash_bytes')();
 
 final genericHashBytesMin =
     sodium.lookupFunction<Uint64 Function(), int Function()>(
-        'crypto_generichash_bytes_min');
+        'crypto_generichash_bytes_min')();
 final genericHashBytesMax =
     sodium.lookupFunction<Uint64 Function(), int Function()>(
-        'crypto_generichash_bytes_max');
+        'crypto_generichash_bytes_max')();
 
 final keyBytes = sodium.lookupFunction<Uint64 Function(), int Function()>(
-    'crypto_generichash_keybytes');
+    'crypto_generichash_keybytes')();
 final keyBytesMax = sodium.lookupFunction<Uint64 Function(), int Function()>(
-    'crypto_generichash_keybytes_max');
+    'crypto_generichash_keybytes_max')();
 final keyBytesMin = sodium.lookupFunction<Uint64 Function(), int Function()>(
-    'crypto_generichash_keybytesMin');
+    'crypto_generichash_keybytesMin')();
 
 final stateBytes = sodium.lookupFunction<Uint64 Function(), int Function()>(
-    'crypto_generichash_statebytes');
+    'crypto_generichash_statebytes')();
 
 typedef _GenericHashNative = Int16 Function(Pointer<Uint8> out, IntPtr outlen,
     Pointer<Uint8> input, Uint64 inlen, Pointer<Uint8> key, IntPtr keylen);
