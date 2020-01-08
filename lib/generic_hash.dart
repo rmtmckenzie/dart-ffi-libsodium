6f331296910519b1fbde55315c1828ef273bcb7d
Marvin Hannott <marvinhannott@gmail.com>
2020-01-08 03:05:27 +0100

    export generic_hash

---
+++ lib/generic_hash.dart
@@ -0,0 +1,9 @@
+export 'src/generic_hash.dart';
+export 'src/bindings/generic_hash.dart'
+    show
+        genericHashBytes,
+        genericHashBytesMax,
+        genericHashBytesMin,
+        keyBytes,
+        keyBytesMax,
+        keyBytesMin;
