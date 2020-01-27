ee442fb4370e08f83f9bea0a551d23e10c2a0704
Marvin Hannott <marvinhannott@gmail.com>
2020-01-28 00:41:29 +0100

    add bindings for key derivation

---
+++ lib/src/bindings/key_derivation.dart
@@ -0,0 +1,28 @@
+import 'sodium.dart';
+import 'dart:ffi';
+
+final subkeyBytesMin = sodium.lookupFunction<IntPtr Function(), int Function()>(
+    'crypto_kdf_bytes_min')();
+
+final subkeyBytesMax = sodium.lookupFunction<IntPtr Function(), int Function()>(
+    'crypto_kdf_bytes_max')();
+final contextBytes = sodium.lookupFunction<IntPtr Function(), int Function()>(
+    'crypto_kdf_contextbytes')();
+final keyBytes = sodium
+    .lookupFunction<IntPtr Function(), int Function()>('crypto_kdf_keybytes')();
+
+final keyGen = sodium.lookupFunction<Void Function(Pointer<Uint8> key),
+    void Function(Pointer<Uint8> key)>('crypto_kdf_keygen');
+
+typedef _DeriveFromKeyNative = Int16 Function(
+    Pointer<Uint8> subkey,
+    IntPtr subkeyLength,
+    Uint64 subkeyId,
+    Pointer<Uint8> context,
+    Pointer<Uint8> key);
+typedef _DeriveFromKeyDart = int Function(Pointer<Uint8> subkey,
+    int subkeyLength, int subkeyId, Pointer<Uint8> context, Pointer<Uint8> key);
+
+final deriveFromKey =
+    sodium.lookupFunction<_DeriveFromKeyNative, _DeriveFromKeyDart>(
+        'crypto_kdf_derive_from_key');
