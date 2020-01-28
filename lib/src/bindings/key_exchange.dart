9eeff8ec897374212e56421e87e4f6d7c28ecdc5
Marvin Hannott <marvinhannott@gmail.com>
2020-01-29 00:26:25 +0100

    add bindings for key_exchange

---
+++ lib/src/bindings/key_exchange.dart
@@ -0,0 +1,47 @@
+import 'sodium.dart';
+import 'dart:ffi';
+
+final keyPair = sodium.lookupFunction<
+    Int16 Function(Pointer<Uint8> publicKey, Pointer<Uint8> secretKey),
+    int Function(Pointer<Uint8> publicKey,
+        Pointer<Uint8> secretKey)>('crypto_kx_keypair');
+
+final seedKeyPair = sodium.lookupFunction<
+    Int16 Function(Pointer<Uint8> publicKey, Pointer<Uint8> secretKey,
+        Pointer<Uint8> seed),
+    int Function(Pointer<Uint8> publicKey, Pointer<Uint8> secretKey,
+        Pointer<Uint8> seed)>('crypto_kx_keypair');
+
+typedef _ClientSessionKeysNative = Int16 Function(
+    Pointer<Uint8> receive,
+    Pointer<Uint8> to,
+    Pointer<Uint8> pk,
+    Pointer<Uint8> sk,
+    Pointer<Uint8> serverPk);
+typedef _ClientSessionKeysDart = int Function(
+    Pointer<Uint8> receive,
+    Pointer<Uint8> to,
+    Pointer<Uint8> pk,
+    Pointer<Uint8> sk,
+    Pointer<Uint8> serverPk);
+
+final clientSessionKeys =
+    sodium.lookupFunction<_ClientSessionKeysNative, _ClientSessionKeysDart>(
+        'crypto_kx_client_session_keys');
+
+typedef _ServerSessionKeysNative = Int16 Function(
+    Pointer<Uint8> receive,
+    Pointer<Uint8> to,
+    Pointer<Uint8> pk,
+    Pointer<Uint8> sk,
+    Pointer<Uint8> clientPk);
+typedef _ServerSessionKeysDart = int Function(
+    Pointer<Uint8> receive,
+    Pointer<Uint8> to,
+    Pointer<Uint8> pk,
+    Pointer<Uint8> sk,
+    Pointer<Uint8> clientPk);
+
+final serverSessionKeys =
+    sodium.lookupFunction<_ClientSessionKeysNative, _ClientSessionKeysDart>(
+        'crypto_kx_server_session_keys');
