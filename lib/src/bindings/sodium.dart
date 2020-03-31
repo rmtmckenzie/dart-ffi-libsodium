25f253e2e466fd6cd1611bd2c31f4b1bb3cee8b5
Marvin Hannott <marvinhannott@gmail.com>
2020-03-31 07:01:33 +0200

    bindings for version string

--- lib/src/bindings/sodium.dart
+++ lib/src/bindings/sodium.dart
@@ -4,3 +4,6 @@ final sodium = DynamicLibrary.open("libsodium");
 
 final init =
     sodium.lookupFunction<Int16 Function(), int Function()>("sodium_init");
+final version =
+    sodium.lookupFunction<Pointer<Uint8> Function(), Pointer<Uint8> Function()>(
+        'sodium_version_string');
