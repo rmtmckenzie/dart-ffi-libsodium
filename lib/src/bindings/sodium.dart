8900062b301fa6a74b091b90543b3ab5f4e7999f
Marvin Hannott <marvinhannott@gmail.com>
2020-03-31 07:14:30 +0200

    squash! bindings for version string

--- lib/src/bindings/sodium.dart
+++ lib/src/bindings/sodium.dart
@@ -6,4 +6,4 @@ final init =
     sodium.lookupFunction<Int16 Function(), int Function()>("sodium_init");
 final version =
     sodium.lookupFunction<Pointer<Uint8> Function(), Pointer<Uint8> Function()>(
-        'sodium_version_string');
+        'sodium_version_string')();
