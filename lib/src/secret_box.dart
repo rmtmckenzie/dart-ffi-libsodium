4da3df4604aaa11266def5674dbb2fe459fbeb59
Marvin Hannott <marvinhannott@gmail.com>
2020-04-03 08:34:58 +0200

    add docs

--- lib/src/secret_box.dart
+++ lib/src/secret_box.dart
@@ -2,74 +2,84 @@ import 'bindings/secretbox.dart' as bindings;
 import 'package:ffi_helper/ffi_helper.dart';
 import 'dart:typed_data';
 import 'internal_helpers.dart';
+import 'random_bytes.dart' as random;
 
-class EncryptionError extends Error {
-  @override
-  String toString() {
-    return 'Failed to encrypt message';
+/// Authenticated encryption of single messages.
+class SecretBox {
+  static const keyBytes = 32;
+  static const nonceBytes = 24;
+
+  /// Generates a key for [SecretBox].
+  static UnmodifiableUint8ListView keyGen() {
+    final keyPtr = Uint8Array.allocate(count: bindings.keyBytes);
+    final key = Uint8List.fromList(keyPtr.view);
+    bindings.keyGen(keyPtr.rawPtr);
+    keyPtr.view.fillZero();
+    keyPtr.free();
+    return UnmodifiableUint8ListView(key);
   }
-}
 
-class DecryptionError extends Error {
-  @override
-  String toString() {
-    return 'Failed to decrypt message';
+  final UnmodifiableUint8ListView key;
+  UnmodifiableUint8ListView nonce;
+  SecretBox._(this.key);
+
+  /// If [key] is left empty, a key will be generated.
+  /// Otherwise [key] must be [keyBytes] long.
+  factory SecretBox([Uint8List key]) {
+    if (key == null) {
+      key = keyGen();
+    } else {
+      checkExpectedLengthOf(key.length, bindings.keyBytes, 'key');
+    }
+    return SecretBox._(UnmodifiableUint8ListView(key));
   }
-}
 
-/// Generates a key for a secret box.
-UnmodifiableUint8ListView keyGen() {
-  final keyPtr = Uint8Array.allocate(count: bindings.keyBytes);
-  final key = Uint8List.fromList(keyPtr.view);
-  bindings.keyGen(keyPtr.rawPtr);
-  keyPtr.view.fillZero();
-  keyPtr.free();
-  return UnmodifiableUint8ListView(key);
-}
-
-/// Encrypts [message] with [key]. [key] must be [keyBytes] long.
-/// [nonce] must a unique value and must be [nonceBytes] long.
-/// Throws [EncryptionError] when encryption fails.
-Uint8List easy(Uint8List message, Uint8List nonce, Uint8List key) {
-  assert(nonce.length == bindings.nonceBytes);
-  assert(key.length == bindings.keyBytes);
-  final messagePtr = Uint8Array.fromTypedList(message);
-  final noncePtr = Uint8Array.fromTypedList(nonce);
-  final keyPtr = Uint8Array.fromTypedList(key);
-  final ciphertextPtr =
-      Uint8Array.allocate(count: message.length + bindings.macBytes);
-  final result = bindings.easy(ciphertextPtr.rawPtr, messagePtr.rawPtr,
-      message.length, noncePtr.rawPtr, keyPtr.rawPtr);
-  messagePtr.free();
-  noncePtr.free();
-  keyPtr.view.fillZero();
-  keyPtr.free();
-  ciphertextPtr.free();
-  if (result != 0) {
-    throw EncryptionError();
+  /// Encrypts [message] with [key].
+  /// If [nonce] is left empty, a nonce will be generated and stored in [SecretBox.nonce].
+  /// Otherwise [nonce] must be [nonceBytes] long.
+  Uint8List encrypt(Uint8List message, [Uint8List nonce]) {
+    if (nonce == null) {
+      nonce = random.buffer(24);
+      this.nonce = nonce;
+    } else {
+      checkExpectedLengthOf(nonce.length, bindings.nonceBytes, 'nonce');
+    }
+    final messagePtr = Uint8Array.fromTypedList(message);
+    final noncePtr = Uint8Array.fromTypedList(nonce);
+    final keyPtr = Uint8Array.fromTypedList(key);
+    final ciphertextPtr =
+        Uint8Array.allocate(count: message.length + bindings.macBytes);
+    final result = bindings.easy(ciphertextPtr.rawPtr, messagePtr.rawPtr,
+        message.length, noncePtr.rawPtr, keyPtr.rawPtr);
+    messagePtr.free();
+    noncePtr.free();
+    keyPtr.view.fillZero();
+    keyPtr.free();
+    ciphertextPtr.free();
+    if (result != 0) {
+      throw Exception();
+    }
+    return Uint8List.fromList(ciphertextPtr.view);
   }
-  return Uint8List.fromList(ciphertextPtr.view);
-}
 
-/// Opens a message encrypted with [easy].
-/// Throws [DecryptionError] when decryption fails.
-Uint8List openEasy(Uint8List ciphertext, Uint8List nonce, Uint8List key) {
-  assert(nonce.length == bindings.nonceBytes);
-  assert(key.length == bindings.keyBytes);
-  final cPtr = Uint8Array.fromTypedList(ciphertext);
-  final noncePtr = Uint8Array.fromTypedList(nonce);
-  final keyPtr = Uint8Array.fromTypedList(key);
-  final messagePtr =
-      Uint8Array.allocate(count: ciphertext.length - bindings.macBytes);
-  final result = bindings.openEasy(messagePtr.rawPtr, cPtr.rawPtr,
-      ciphertext.length, noncePtr.rawPtr, keyPtr.rawPtr);
-  cPtr.free();
-  noncePtr.free();
-  keyPtr.view.fillZero();
-  keyPtr.free();
-  messagePtr.free();
-  if (result != 0) {
-    throw DecryptionError();
+  /// Decrypts a message encrypted with [encrypt].
+  Uint8List decrypt(Uint8List ciphertext, Uint8List nonce) {
+    checkExpectedLengthOf(nonce.length, bindings.nonceBytes, 'nonce');
+    final cPtr = Uint8Array.fromTypedList(ciphertext);
+    final noncePtr = Uint8Array.fromTypedList(nonce);
+    final keyPtr = Uint8Array.fromTypedList(key);
+    final messagePtr =
+        Uint8Array.allocate(count: ciphertext.length - bindings.macBytes);
+    final result = bindings.openEasy(messagePtr.rawPtr, cPtr.rawPtr,
+        ciphertext.length, noncePtr.rawPtr, keyPtr.rawPtr);
+    cPtr.free();
+    noncePtr.free();
+    keyPtr.view.fillZero();
+    keyPtr.free();
+    messagePtr.free();
+    if (result != 0) {
+      throw Exception();
+    }
+    return Uint8List.fromList(messagePtr.view);
   }
-  return Uint8List.fromList(messagePtr.view);
 }
