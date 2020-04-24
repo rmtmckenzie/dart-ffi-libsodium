50515bbdec6324958adc3ff4f97f176d49ad0031
Marvin Hannott <marvinhannott@gmail.com>
2020-04-24 18:08:30 +0200

    implement pwhash methods

--- lib/src/bindings/pwhash.dart
+++ lib/src/bindings/pwhash.dart
@@ -1,24 +1,4 @@
 import 'dart:ffi';
-import 'sodium.dart';
-
-typedef _StoreNative = Int16 Function(Pointer<Uint8> out, Pointer<Uint8> passwd,
-    Uint64 passwdLen, Uint64 opsLimit, IntPtr memlimit);
-typedef _StoreDart = int Function(Pointer<Uint8> out, Pointer<Uint8> passwd,
-    int passwdLen, int opsLimit, int memlimit);
-final store =
-    sodium.lookupFunction<_StoreNative, _StoreDart>("crypto_pwhash_str");
-
-typedef _VerifyNative = Int16 Function(
-    Pointer<Uint8> str, Pointer<Uint8> passwd, IntPtr passwdlen);
-typedef _VerifyDart = int Function(
-    Pointer<Uint8> str, Pointer<Uint8> passwd, int passwdlen);
-final verify = sodium
-    .lookupFunction<_VerifyNative, _VerifyDart>("crypto_pwhash_str_verify");
-
-final needsRehash = sodium.lookupFunction<
-    Int16 Function(Pointer<Uint8> str, Uint64 opsLimit, IntPtr memLimit),
-    int Function(Pointer<Uint8> str, int opsLimit,
-        int memLimit)>("crypto_pwhash_str_needs_rehash");
 
 class OpsLimit {
   OpsLimit(DynamicLibrary sodium)
@@ -60,6 +40,21 @@ class MemLimit {
   final int max;
 }
 
+typedef PwhashStorageNative = Int8 Function(Pointer<Uint8> out,
+    Pointer<Uint8> passwd, Uint64 passwdLen, Uint64 opsLimit, IntPtr memlimit);
+typedef PwhashStorageDart = int Function(Pointer<Uint8> out,
+    Pointer<Uint8> passwd, int passwdLen, int opsLimit, int memlimit);
+
+typedef PwhashStorageVerifyNative = Int8 Function(
+    Pointer<Uint8> str, Pointer<Uint8> passwd, IntPtr passwdlen);
+typedef PwhashStorageVerifyDart = int Function(
+    Pointer<Uint8> str, Pointer<Uint8> passwd, int passwdlen);
+
+typedef PwhashNeedsRehashNative = Int8 Function(
+    Pointer<Uint8> str, Uint64 opsLimit, IntPtr memLimit);
+typedef PwhashNeedsRehashDart = int Function(
+    Pointer<Uint8> str, int opsLimit, int memLimit);
+
 class PasswordHash {
   PasswordHash(DynamicLibrary sodium)
       : opslimit = OpsLimit(sodium),
@@ -69,11 +64,26 @@ class PasswordHash {
         passwordMax = sodium.lookupFunction<Uint64 Function(), int Function()>(
             'crypto_pwhash_passwd_max')(),
         passwordMin = sodium.lookupFunction<Uint64 Function(), int Function()>(
-            'crypto_pwhash_passwd_min')();
+            'crypto_pwhash_passwd_min')(),
+        storage = sodium
+            .lookup<NativeFunction<PwhashStorageNative>>('crypto_pwhash_str')
+            .asFunction(),
+        verify = sodium
+            .lookup<NativeFunction<PwhashStorageVerifyNative>>(
+                'crypto_pwhash_str_verify')
+            .asFunction(),
+        needsRehash = sodium
+            .lookup<NativeFunction<PwhashNeedsRehashNative>>(
+                'crypto_pwhash_str_needs_rehash')
+            .asFunction();
 
   final OpsLimit opslimit;
   final MemLimit memlimit;
   final int storeBytes;
   final int passwordMax;
   final int passwordMin;
+
+  final PwhashStorageDart storage;
+  final PwhashStorageVerifyDart verify;
+  final PwhashNeedsRehashDart needsRehash;
 }
