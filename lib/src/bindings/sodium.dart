9723d322b2b673056a416d2ccc0724fd586ca2c0
Marvin Hannott <marvinhannott@gmail.com>
2020-04-24 22:19:29 +0200

    remove sodium library
    replaced with libsodium wrapper class

--- lib/src/bindings/sodium.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:ffi';
-
-final sodium = DynamicLibrary.open("libsodium");
-
-final init =
-    sodium.lookupFunction<Int16 Function(), int Function()>("sodium_init");
-final version =
-    sodium.lookupFunction<Pointer<Uint8> Function(), Pointer<Uint8> Function()>(
-        'sodium_version_string')();
