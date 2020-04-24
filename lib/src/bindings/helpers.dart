0c3bfe53d9c67d9e6d93239fb6e1ba5b93857e8f
Marvin Hannott <marvinhannott@gmail.com>
2020-04-24 22:14:32 +0200

    remove helper library

--- lib/src/bindings/helpers.dart
+++
@@ -1,9 +0,0 @@
-import 'sodium.dart';
-import 'dart:ffi';
-
-typedef _MemCmpNative = Int16 Function(
-    Pointer<Uint8> b1, Pointer<Uint8> b2, IntPtr len);
-typedef _MemCmpDart = int Function(
-    Pointer<Uint8> b1, Pointer<Uint8> b2, int len);
-final memoryCompare =
-    sodium.lookupFunction<_MemCmpNative, _MemCmpDart>("sodium_memcmp");
