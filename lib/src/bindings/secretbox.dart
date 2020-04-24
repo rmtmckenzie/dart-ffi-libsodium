c4dfc0febbe393d97979d984ce781ebfe6dde824
Marvin Hannott <marvinhannott@gmail.com>
2020-04-24 19:25:41 +0200

    implement SecretBox wrapper class

--- lib/src/bindings/secretbox.dart
+++ lib/src/bindings/secretbox.dart
@@ -1,42 +1,87 @@
-import 'sodium.dart';
 import 'dart:ffi';
 
-typedef _EasyNative = Int16 Function(
-    Pointer<Uint8> cyphertext,
+typedef SecretBoxEasyNative = Int8 Function(
+    Pointer<Uint8> ciphertext,
     Pointer<Uint8> msg,
     Uint64 msglen,
     Pointer<Uint8> nonce,
     Pointer<Uint8> key);
-typedef _EasyDart = int Function(Pointer<Uint8> cyphertext, Pointer<Uint8> msg,
-    int msglen, Pointer<Uint8> nonce, Pointer<Uint8> key);
-final easy =
-    sodium.lookupFunction<_EasyNative, _EasyDart>("crypto_secretbox_easy");
+typedef SecretBoxEasyDart = int Function(Pointer<Uint8> ciphertext,
+    Pointer<Uint8> msg, int msglen, Pointer<Uint8> nonce, Pointer<Uint8> key);
 
-typedef _OpenEasyNative = Int16 Function(
+typedef SecretBoxOpenEasyNative = Int8 Function(
     Pointer<Uint8> msg,
     Pointer<Uint8> cypherText,
     Uint64 cypherTextLen,
     Pointer<Uint8> nonce,
     Pointer<Uint8> key);
-typedef _OpenEasyDart = int Function(
+typedef SecretBoxOpenEasyDart = int Function(
     Pointer<Uint8> msg,
     Pointer<Uint8> cypherText,
     int cypherTextLen,
     Pointer<Uint8> nonce,
     Pointer<Uint8> key);
-final openEasy = sodium.lookupFunction<_OpenEasyNative, _OpenEasyDart>(
-    "crypto_secretbox_open_easy");
 
-final macBytes = sodium.lookupFunction<Uint64 Function(), int Function()>(
-    "crypto_secretbox_macbytes")();
+typedef SecretBoxDetachedNative = Int8 Function(
+    Pointer<Uint8> ctext,
+    Pointer<Uint8> mac,
+    Pointer<Uint8> msg,
+    Uint64 mlen,
+    Pointer<Uint8> nonce,
+    Pointer<Uint8> key);
+typedef SecretBoxDetachedDart = int Function(
+    Pointer<Uint8> ctext,
+    Pointer<Uint8> mac,
+    Pointer<Uint8> msg,
+    int mlen,
+    Pointer<Uint8> nonce,
+    Pointer<Uint8> key);
 
-/// Required length of [key]
-final keyBytes = sodium.lookupFunction<Uint64 Function(), int Function()>(
-    "crypto_secretbox_keybytes")();
+typedef SecretBoxOpenDetachedNative = Int8 Function(
+    Pointer<Uint8> msg,
+    Pointer<Uint8> ctext,
+    Pointer<Uint8> mac,
+    Uint64 clen,
+    Pointer<Uint8> nonce,
+    Pointer<Uint8> key);
+typedef SecretBoxOpenDetachedDart = int Function(Pointer<Uint8>, Pointer<Uint8>,
+    Pointer<Uint8>, int, Pointer<Uint8>, Pointer<Uint8>);
 
-/// Required length of [nonce]
-final nonceBytes = sodium.lookupFunction<Uint64 Function(), int Function()>(
-    "crypto_secretbox_noncebytes")();
+class SecretBox {
+  SecretBox(DynamicLibrary sodium)
+      : macBytes = sodium.lookupFunction<Uint64 Function(), int Function()>(
+            'crypto_secretbox_macbytes')(),
+        keyBytes = sodium.lookupFunction<Uint64 Function(), int Function()>(
+            'crypto_secretbox_keybytes')(),
+        nonceBytes = sodium.lookupFunction<Uint64 Function(), int Function()>(
+            'crypto_secretbox_noncebytes')(),
+        keygen = sodium
+            .lookup<NativeFunction<Void Function(Pointer<Uint8>)>>(
+                'crypto_secretbox_keygen')
+            .asFunction(),
+        easy = sodium
+            .lookup<NativeFunction<SecretBoxEasyNative>>(
+                'crypto_secretbox_easy')
+            .asFunction(),
+        openEasy = sodium
+            .lookup<NativeFunction<SecretBoxEasyNative>>(
+                'crypto_secretbox_open_easy')
+            .asFunction(),
+        detached = sodium
+            .lookup<NativeFunction<SecretBoxDetachedNative>>(
+                'crytpo_secretbox_detached')
+            .asFunction(),
+        openDetached = sodium
+            .lookup<NativeFunction<SecretBoxOpenDetachedNative>>(
+                'crytpo_secretbox_open_detached')
+            .asFunction();
 
-final keyGen = sodium.lookupFunction<Void Function(Pointer<Uint8>),
-    void Function(Pointer<Uint8>)>("crypto_secretbox_keygen");
+  final int macBytes;
+  final int keyBytes;
+  final int nonceBytes;
+  final void Function(Pointer<Uint8> key) keygen;
+  final SecretBoxEasyDart easy;
+  final SecretBoxOpenEasyDart openEasy;
+  final SecretBoxDetachedDart detached;
+  final SecretBoxOpenDetachedDart openDetached;
+}
