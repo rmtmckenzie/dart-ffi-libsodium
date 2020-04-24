8d00435c29e36bc4b4f836e44b860cb03063f4eb
Marvin Hannott <marvinhannott@gmail.com>
2020-04-24 20:06:13 +0200

    implement SecretStream wrapper class

--- lib/src/bindings/secretstream.dart
+++ lib/src/bindings/secretstream.dart
@@ -6,95 +6,110 @@ final keyGen = sodium.lookupFunction<
     void Function(
         Pointer<Uint8> key)>("crypto_secretstream_xchacha20poly1305_keygen");
 
-abstract class Tag {
-  static final message =
-      sodium.lookupFunction<Uint8 Function(), int Function()>(
-          "crypto_secretstream_xchacha20poly1305_tag_message")();
-  static final finish = sodium.lookupFunction<Uint8 Function(), int Function()>(
-      "crypto_secretstream_xchacha20poly1305_tag_final")();
-  static final push = sodium.lookupFunction<Uint8 Function(), int Function()>(
-      "crypto_secretstream_xchacha20poly1305_tag_push")();
-  static final rekey = sodium.lookupFunction<Uint8 Function(), int Function()>(
-      "crypto_secretstream_xchacha20poly1305_tag_rekey")();
+class Tag {
+  Tag(DynamicLibrary sodium)
+      : message = sodium.lookupFunction<Uint8 Function(), int Function()>(
+            'crypto_secretstream_xchacha20poly1305_tag_message')(),
+        finish = sodium.lookupFunction<Uint8 Function(), int Function()>(
+            'crypto_secretstream_xchacha20poly1305_tag_final')(),
+        push = sodium.lookupFunction<Uint8 Function(), int Function()>(
+            'crypto_secretstream_xchacha20poly1305_tag_push')(),
+        rekey = sodium.lookupFunction<Uint8 Function(), int Function()>(
+            'crypto_secretstream_xchacha20poly1305_tag_rekey')();
+  final int message;
+  final int finish;
+  final int push;
+  final int rekey;
 }
 
-final initPush = sodium.lookupFunction<
-    Int16 Function(
-        Pointer<Uint8> state, Pointer<Uint8> header, Pointer<Uint8> key),
-    int Function(Pointer<Uint8> state, Pointer<Uint8> header,
-        Pointer<Uint8> key)>("crypto_secretstream_xchacha20poly1305_init_push");
+typedef SecretStreamInitPushPullNative = Int8 Function(
+    Pointer<Uint8> state, Pointer<Uint8> header, Pointer<Uint8> key);
+typedef SecretStreamInitPushPullDart = int Function(
+    Pointer<Uint8> state, Pointer<Uint8> header, Pointer<Uint8> key);
 
-typedef _PushNative = Int16 Function(
+typedef SecretStreamPushNative = Int8 Function(
     Pointer<Uint8> state,
-    Pointer<Uint8> ciphertext,
+    Pointer<Uint8> ctext,
     Pointer<Uint64> clen,
     Pointer<Uint8> msg,
     Uint64 mlen,
     Pointer<Uint8> adData,
     Uint64 adlen,
-    Uint8 tag);
-typedef _PushDart = int Function(
+    Pointer<Uint8> tag);
+typedef SecretStreamPushDart = int Function(
     Pointer<Uint8> state,
-    Pointer<Uint8> ciphertext,
+    Pointer<Uint8> ctext,
     Pointer<Uint64> clen,
     Pointer<Uint8> msg,
     int mlen,
     Pointer<Uint8> adData,
     int adlen,
-    int tag);
+    Pointer<Uint8> tag);
 
-final push = sodium.lookupFunction<_PushNative, _PushDart>(
-    "crypto_secretstream_xchacha20poly1305_push");
-
-typedef _PullNative = Int16 Function(
+typedef SecretStreamPullNative = Int8 Function(
     Pointer<Uint8> state,
     Pointer<Uint8> msg,
     Pointer<Uint64> msglen,
     Pointer<Uint8> tag,
-    Pointer<Uint8> ctxt,
+    Pointer<Uint8> ctext,
     Uint64 clen,
-    Pointer<Uint8> addData,
+    Pointer<Uint8> adData,
     Uint64 adlen);
-
-typedef _PullDart = int Function(
+typedef SecretStreamPullDart = int Function(
     Pointer<Uint8> state,
     Pointer<Uint8> msg,
     Pointer<Uint64> msglen,
     Pointer<Uint8> tag,
-    Pointer<Uint8> ctxt,
+    Pointer<Uint8> ctext,
     int clen,
-    Pointer<Uint8> addData,
+    Pointer<Uint8> adData,
     int adlen);
-final pull = sodium.lookupFunction<_PullNative, _PullDart>(
-    "crypto_secretstream_xchacha20poly1305_pull");
 
-final initPull = sodium.lookupFunction<
-    Int16 Function(
-        Pointer<Uint8> state, Pointer<Uint8> header, Pointer<Uint8> key),
-    int Function(Pointer<Uint8> state, Pointer<Uint8> header,
-        Pointer<Uint8> key)>("crypto_secretstream_xchacha20poly1305_init_pull");
+class SecretStream {
+  SecretStream(DynamicLibrary sodium)
+      : keyBytes = sodium.lookupFunction<Uint64 Function(), int Function()>(
+            'crypto_secretstream_xchacha20poly1305_keybytes')(),
+        stateBytes = sodium.lookupFunction<Uint64 Function(), int Function()>(
+            'crypto_secretstream_xchacha20poly1305_statebytes')(),
+        headerBytes = sodium.lookupFunction<Uint64 Function(), int Function()>(
+            'crypto_secretstream_xchacha20poly1305_headerbytes')(),
+        messageBytesMax =
+            sodium.lookupFunction<Uint64 Function(), int Function()>(
+                'crypto_secretstream_xchacha20poly1305_messagebytes_max')(),
+        keygen = sodium
+            .lookup<NativeFunction<Void Function(Pointer<Uint8>)>>(
+                'crypto_secretstream_xchacha20poly1305_keygen')
+            .asFunction(),
+        initPush = sodium
+            .lookup<NativeFunction<SecretStreamInitPushPullNative>>(
+                'crypto_secretstream_xchacha20poly1305_init_push')
+            .asFunction(),
+        initPull = sodium
+            .lookup<NativeFunction<SecretStreamInitPushPullNative>>(
+                'crypto_secretstream_xchacha20poly1305_init_pull')
+            .asFunction(),
+        push = sodium
+            .lookup<NativeFunction<SecretStreamPushNative>>(
+                'crypto_secretstream_xchacha20poly1305_push')
+            .asFunction(),
+        pull = sodium
+            .lookup<NativeFunction<SecretStreamPullNative>>(
+                'crypto_secretstream_xchacha20poly1305_pull')
+            .asFunction(),
+        rekey = sodium
+            .lookup<NativeFunction<Void Function(Pointer<Uint8> state)>>(
+                'crypto_secretstream_xchacha20poly1305_rekey')
+            .asFunction();
 
-final rekey = sodium.lookupFunction<
-    Void Function(Pointer<Uint8> state),
-    void Function(
-        Pointer<Uint8> state)>("crypto_secretstream_xchacha20poly1305_rekey");
-
-final keyBytes = sodium.lookupFunction<Uint64 Function(), int Function()>(
-    "crypto_secretstream_xchacha20poly1305_keybytes")();
-
-final aBytes = sodium.lookupFunction<Uint64 Function(), int Function()>(
-    "crypto_secretstream_xchacha20poly1305_abytes")();
-
-final stateBytes = sodium.lookupFunction<Uint64 Function(), int Function()>(
-    "crypto_secretstream_xchacha20poly1305_statebytes")();
-final headerBytes = sodium.lookupFunction<Uint64 Function(), int Function()>(
-    "crypto_secretstream_xchacha20poly1305_headerbytes")();
-final msgBytesMax = sodium.lookupFunction<Uint64 Function(), int Function()>(
-    "crypto_secretstream_xchacha20poly1305_messagebytes_max")();
-
-final chacha20IetfNonceBytes =
-    sodium.lookupFunction<Uint64 Function(), int Function()>(
-        "crypto_stream_chacha20_ietf_noncebytes")();
-final chacha20IetfKeyBytes =
-    sodium.lookupFunction<Uint64 Function(), int Function()>(
-        "crypto_stream_chacha20_ietf_keybytes")();
+  final int keyBytes;
+  final int stateBytes;
+  final int headerBytes;
+  final int messageBytesMax;
+  final void Function(Pointer<Uint8> key) keygen;
+  final SecretStreamInitPushPullDart initPush;
+  final SecretStreamPushDart push;
+  final SecretStreamInitPushPullDart initPull;
+  final SecretStreamPullDart pull;
+  final void Function(Pointer<Uint8> state) rekey;
+  final tag = Tag(sodium);
+}
